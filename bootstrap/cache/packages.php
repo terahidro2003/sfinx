35939152fa59cb4aeb3a9e89c00a9faf76931fa4
terahidro2003 <juozas2003@gmail.com>
2018-08-04 12:03:45 +0300

    Add files via upload

---
+++ bootstrap/cache/packages.php
@@ -0,0 +1,41 @@
+<?php return array (
+  'davejamesmiller/laravel-breadcrumbs' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'DaveJamesMiller\\Breadcrumbs\\BreadcrumbsServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Breadcrumbs' => 'DaveJamesMiller\\Breadcrumbs\\Facades\\Breadcrumbs',
+    ),
+  ),
+  'fideloper/proxy' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Fideloper\\Proxy\\TrustedProxyServiceProvider',
+    ),
+  ),
+  'laravel-frontend-presets/uikit3' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'LaravelFrontendPresets\\UIKit3Preset\\UIKit3PresetServiceProvider',
+    ),
+  ),
+  'laravel/scout' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Scout\\ScoutServiceProvider',
+    ),
+  ),
+  'laravel/tinker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Tinker\\TinkerServiceProvider',
+    ),
+  ),
+);
